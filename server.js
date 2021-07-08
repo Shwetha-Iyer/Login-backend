f49141bf41381eaf0c58da6a8894bbee1551aa95
Shwetha-Iyer <shwetharaj555@gmail.com>
2021-07-08 17:18:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const router = express.Router();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+const {
+    HOST,
+    PORT,
+    SESS_SECRET,
+    NODE_ENV,
+    IS_PROD,
+    COOKIE_NAME
+  } = require("./config/config");
+const { MongoURI } = require("./config/database");
+const MAX_AGE = 1000 * 60 * 60 * 3; // Three hours
+
+var whitelist = ["http://localhost:3000"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials:true
+}
+app.use(cors(corsOptions));
+
+// Connecting to Database
+mongoose
+  .connect(MongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+// setting up connect-mongodb-session store
+const mongoDBstore = new MongoDBStore({
+  uri: MongoURI,
+  collection: "mySessions"
+});
+
+// Express Bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Express-Session
+app.use(
+  session({
+    name: COOKIE_NAME, //name to be put in "key" field in postman etc
+    secret: SESS_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: mongoDBstore,
+    cookie: {
+      maxAge: MAX_AGE,
+      sameSite: "none",
+      secure: false
+    }
+  })
+);
+
+app.use("/api/users", require("./routes/users"));
+
+app.listen(PORT, () => console.log(`Server started on http://${HOST}:${PORT}`));
